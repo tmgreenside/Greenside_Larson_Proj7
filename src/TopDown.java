4bc87b3d3d03cdf4ccbb15bbeb3b9b68e2d44113
tmgreenside <trevmg7@gmail.com>
2018-03-27 16:25:43 -0700

    Created TopDown class file

--- src/TopDown.java
+++ src/TopDown.java
@@ -1,3 +1,22 @@
-public class TopDown {
 
+import java.util.ArrayList;
+
+public class TopDown {
+    
+    ArrayList<City[]> paths;
+    ArrayList<Double> pathLengths;
+    
+    public void addArrays(City[] path) {
+        paths.add(path);
+    }
+    
+    public void elimination() {
+        Cities cities = new Cities();
+        
+        for (City[] path : paths) {
+            pathLengths.add(cities.calculateDistance(path));
+        }
+        
+        
+    }
 }
