f00d96d56c0529a97d9ffd2161dfae00c3e2f1b6
kaylaLarson2018 <37782349+kaylaLarson2018@users.noreply.github.com>
2018-03-28 16:17:16 -0700

    Update GA.java

--- src/GA.java
+++ src/GA.java
@@ -7,6 +7,32 @@ public class GA {
     public void addArrays(City[] path){
         paths.add(path);
     }
+    public void generateFirstSetOfPaths(){
+        City[] parent1 = new City[10];
+        parent1[0] = (new City(50,50));
+        parent1[1] = (new City(20, 89));
+        parent1[2] = (new City(50, 60));
+        parent1[3] = (new City(28,35));
+        parent1[4] = (new City(20,79));
+        parent1[5] = (new City(69, 30));
+        parent1[6] = (new City(48,98));
+        parent1[7] = (new City(30,99));
+        parent1[8] = (new City(29,80));
+        parent1[9] = (new City(40,20));
+        paths.add(parent1);
+        int counter = 0;
+        while(counter < 50){
+            City[] parent2 = new City[10];
+            parent2 = parent1.clone();
+            Collections.shuffle(Arrays.asList(parent2));
+            if(!Arrays.asList(parent2).contains(parent2)){
+                paths.add(parent2);
+                counter++;
+                System.out.println(Arrays.toString(parent2));
+            }
+        }
+    }
+
     //top down
     public void generateChildrenDoublePoint(){
         Children childSet;
@@ -20,6 +46,10 @@ public class GA {
                 paths.add(counter,childSet.getSecondChild());
                 counter = counter + 2;
             }
+        }
+        System.out.println("*******************************************************************************************************");
+        for (City[] path:paths) {
+            System.out.println(Arrays.toString(path));
         }
     }
     //top down
@@ -47,14 +77,17 @@ public class GA {
                 double distance1 = cities.calculateDistance(paths.get(j));
                 double distance2 = cities.calculateDistance(paths.get(j + 1));
                 if(distance1 > distance2){
-                    City[] temp = paths.get(j);
-
+                    City[] temp = Arrays.copyOf(paths.get(j), 10);
+                    paths.set(j, paths.get(j+1));
+                    paths.set(j+1, temp);
                 }
             }
-
-
         }
-    }
+        for (City[] path:paths) {
+            if(paths.indexOf(path) < size){
+                paths.remove(path);
+            }
+        }
     
     /*
     Description of algorithm from De Palma's slides:
